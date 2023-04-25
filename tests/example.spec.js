64a13df59d9c66261b70e7c471c79b8c50d99f93
Evilmoose <ronnielopez@gmail.com>
2023-04-25 13:04:49 -0700

    Clicking on elements

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -8,3 +8,12 @@ test("Simple basic test", async ({ page }) => {
     await expect(pageTitle).toContainText('Example Domain');    // Called the stored element to use 'expect' funtion.
 });
 
+test("Clicking on Elements", async ({ page }) => {
+    await page.goto("https://www.oreilly.com/");
+    await page.click("#nav-signIn");
+    await page.click('text=Continue');
+
+    const errorMessage = await page.locator('.orm-Input-errorTxt');
+    await expect(errorMessage).toContainText('Invalid email address.');
+
+})
